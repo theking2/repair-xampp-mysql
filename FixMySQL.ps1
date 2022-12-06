476a9ece9097a97968eb03e98c7dd73ea50a5df8
theking2 <theking2@king.ma>
2022-12-06 10:46:20 +0100

    First release
    Not checking if mysql is running 😱

---
+++ FixMySQL.ps1
@@ -0,0 +1,21 @@
+﻿$OldData = "C:\xampp\mysql\data_$(Get-Date -Format "yyyyMMdd")"
+$Data = "C:\xampp\mysql\data"
+$Ignore = "mysql","performance_schema","phpmyadmin","test"
+
+Move-Item $Data $OldData
+Copy-Item -Recurse C:\xampp\mysql\backup $Data
+
+$SubFolders = Get-ChildItem -Path $OldData -Directory
+ForEach($d in $SubFolders) {
+    if($Ignore -Contains $d.Name) {
+        Write-Output "Skip "$d.FullName
+    } else {
+        Write-Output "Copy "$d.FullName
+        Copy-Item -Recurse $d.FullName $Data
+    }
+}
+
+Copy-Item $OldData\ib_logfile0 $Data
+Copy-Item $OldData\ib_logfile1 $Data
+Copy-Item $OldData\ibdata1 $Data
+
